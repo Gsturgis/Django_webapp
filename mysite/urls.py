eb88fa9c9c8e1234208b39f6cb7e03512a6b3315
Gyasi Sturgis <Gsillymac93@Gyasis-MacBook-Pro.local>
2021-04-22 02:45:32 -0400

    created about and index views

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('about/', views.about, name='about'),
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
+
+# view using render()
 def index(request):
-    return HttpResponse('Hello World')
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
