32fa1a639edf901fac90ca47580ae182a0033685
Gyasi Sturgis <Gsillymac93@Gyasis-MacBook-Pro.local>
2021-05-06 15:32:41 -0400

    views,templates,requirements added

--- mysite/.ipynb_checkpoints/urls-checkpoint.py
+++ mysite/.ipynb_checkpoints/urls-checkpoint.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import include, path
+from .views import index, about
 
 urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('', views.index, name='index'),
+    path('about/', about),
+    path('', index),
     path('admin/', admin.site.urls),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,17 +1,16 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-from mysite.models import Person
+from .models import Person, Orders
 
 
 
 # view using render()
 def index(request):
-    data = {
-        "person": Person.objects.all(),      
-    }
-    print(data)
-    return render(request, 'index.html', data)
+    person = Person.objects.all(),      
+    
+    
+    return render(request, 'index.html', context={'person':person})
 
 def about(request):
     return render(request, 'about.html')
