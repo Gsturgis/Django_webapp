e46c8a424eaaf02f3d97fc49d2c747508e470332
Gyasi Sturgis <Gsillymac93@Gyasis-MacBook-Pro.local>
2021-05-08 02:56:59 -0400

    forms.py

--- mysite/.ipynb_checkpoints/views-checkpoint.py
+++ mysite/.ipynb_checkpoints/views-checkpoint.py
@@ -1,16 +1,22 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+
 from .models import Person, Orders
+from .forms import UserForm
 
 
 
 # view using render()
 def index(request):
-    person = Person.objects.all(),      
-    
-    
-    return render(request, 'index.html', context={'person':person})
+    users = Person.objects.all()
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserForm
+    return render(request, 'index.html', context={'users':users, 'form': form})
 
 def about(request):
     return render(request, 'about.html')
--- mysite/models.py
+++ mysite/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Person(models.Model):
+    email = models.EmailField(max_length=45, unique=False, null=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     
